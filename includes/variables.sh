85082629b64b1eef90d8cdc6be6d95eed0d40df8
exrat <ex_rat@hotmail.com>
2016-09-16 07:22:39 +0200

    upgrade nodejs 6.6.0 & nvm 0.32.0

--- includes/variables.sh
+++ includes/variables.sh
@@ -12,8 +12,8 @@ VERSION=$(cat /etc/debian_version)
 LIBTORRENT="0.13.6"
 RTORRENT="0.9.6"
 #DEBMULTIMEDIA="2016.8.1"
-NVM="0.31.7"
-NODE="6.5.0"
+NVM="0.32.0"
+NODE="6.6.0"
 
 RUTORRENT="/var/www/rutorrent"
 RUPLUGINS="/var/www/rutorrent/plugins"
