a1444c1c5f40e36e249bc5a818c5ddc136272950
exrat <ex_rat@hotmail.com>
2016-11-25 19:36:18 +0100

    typo

--- includes/langues.sh
+++ includes/langues.sh
@@ -4,26 +4,29 @@
 OPTS=$(getopt -o vhns: --long en,fr,it,de,ru,es,pt -n 'parse-options' -- "$@")
 eval set -- "$OPTS"
 while true; do
-  case "$1" in
-	--en) GENLANG="en" ; break ;;
-	--fr) GENLANG="fr" ; break ;;
-	--de) GENLANG="de" ; break ;;
-	--ru) GENLANG="ru" ; break ;;
-	--es) GENLANG="es" ; break ;;
-	--pt) GENLANG="pt" ; break ;;
-	--ptbr) GENLANG="ptbr" ; break ;;
-	*|\?)
-		BASELANG="${LANG:0:2}"
-		# detection auto
-		if   [ "$BASELANG" = "en" ]; then GENLANG="en"
-		elif [ "$BASELANG" = "fr" ]; then GENLANG="fr"
-		elif [ "$BASELANG" = "de" ]; then GENLANG="de"
-		elif [ "$BASELANG" = "ru" ]; then GENLANG="ru"
-		elif [ "$BASELANG" = "es" ]; then GENLANG="es"
-		elif [ "$BASELANG" = "pt" ]; then GENLANG="pt"
-		elif [ "$BASELANG" = "pt_BR" ]; then GENLANG="ptbr"
-		else
-			GENLANG="en" ; fi ; break ;;
+	case "$1" in
+			--en) GENLANG="en" ; break ;;
+			--fr) GENLANG="fr" ; break ;;
+			--de) GENLANG="de" ; break ;;
+			--ru) GENLANG="ru" ; break ;;
+			--es) GENLANG="es" ; break ;;
+			--pt) GENLANG="pt" ; break ;;
+			--ptbr) GENLANG="ptbr" ; break ;;
+		*|\?)
+			BASELANG="${LANG:0:2}"
+			# detection auto
+			if   [ "$BASELANG" = "en" ]; then GENLANG="en"
+			elif [ "$BASELANG" = "fr" ]; then GENLANG="fr"
+			elif [ "$BASELANG" = "de" ]; then GENLANG="de"
+			elif [ "$BASELANG" = "ru" ]; then GENLANG="ru"
+			elif [ "$BASELANG" = "es" ]; then GENLANG="es"
+			elif [ "$BASELANG" = "pt" ]; then GENLANG="pt"
+			elif [ "$BASELANG" = "pt_BR" ]; then GENLANG="ptbr"
+			else
+				GENLANG="en"
+			fi
+			break
+		;;
 	esac
 done
 
