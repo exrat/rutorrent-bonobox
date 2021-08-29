ed6d56f172b0c8255ab77ef5632c6c8bba47e01e
mog54 <petitdalouzy@hotmail.fr>
2021-08-29 20:38:40 +0200

    Update cmd.sh

--- includes/cmd.sh
+++ includes/cmd.sh
@@ -4,70 +4,70 @@ CMDPATH=$(/usr/bin/lsb_release -cs)
 
 
 # Youre distribution can be upgrated to buster but still have the program runing in /bin
-if [[ "$CMDPATH" == stretch ]]  || [ ! -f "/usr/bin/bash" ]; then
-        # variables cmd stretch
+if [[ "$CMDPATH" == buster ]]  || [ ! -f "/usr/bin/bash" ]; then
+        # variables cmd buster
         CMDAPTGET="/usr/bin/apt-get"
         CMDAPTKEY="/usr/bin/apt-key"
         CMDAWK="/usr/bin/awk"
-        CMDBASH="/bin/bash" #
-        CMDBZIP2="/bin/bzip2" #
-        CMDCAT="/bin/cat" #
+        CMDBASH="/usr/bin/bash" #
+        CMDBZIP2="/usr/bin/bzip2" #
+        CMDCAT="/usr/bin/cat" #
         CMDCCZE="/usr/bin/ccze"
-        CMDCHMOD="/bin/chmod" #
-        CMDCHOWN="/bin/chown" #
+        CMDCHMOD="/usr/bin/chmod" #
+        CMDCHOWN="/usr/bin/chown" #
         CMDCHPASSWD="/usr/sbin/chpasswd"
         CMDCLEAR="/usr/bin/clear"
-        CMDCP="/bin/cp" #
+        CMDCP="/usr/bin/cp" #
         CMDCRONTAB="/usr/bin/crontab"
         CMDCURL="/usr/bin/curl"
         CMDCUT="/usr/bin/cut"
-        CMDDATE="/bin/date" #
+        CMDDATE="/usr/bin/date" #
         CMDDELUSER="/usr/sbin/deluser"
-        CMDDF="/bin/df" #
+        CMDDF="/usr/bin/df" #
         CMDDPKG="/usr/bin/dpkg"
-        CMDECHO="/bin/echo" #
+        CMDECHO="/usr/bin/echo" #
         CMDGETOPT="/usr/bin/getopt"
         CMDGIT="/usr/bin/git"
-        CMDGREP="/bin/grep" #
-        CMDGZIP="/bin/gzip" #
+        CMDGREP="/usr/bin/grep" #
+        CMDGZIP="/usr/bin/gzip" #
         CMDHEAD="/usr/bin/head"
         CMDHTPASSWD="/usr/bin/htpasswd"
         CMDID="/usr/bin/id"
-        CMDIP="/bin/ip" #
-        CMDKILL="/bin/kill" #
-        CMDLDCONFIG="/sbin/ldconfig" #
-        CMDLN="/bin/ln" #
-        CMDLS="/bin/ls" #
+        CMDIP="/usr/bin/ip" #
+        CMDKILL="/usr/bin/kill" #
+        CMDLDCONFIG="/usr/sbin/ldconfig" #
+        CMDLN="/usr/bin/ln" #
+        CMDLS="/usr/bin/ls" #
         CMDMAKE="/usr/bin/make"
-        CMDMKDIR="/bin/mkdir" #
-        CMDMOUNT="/bin/mount" #
-        CMDMV="/bin/mv" #
-        CMDNETSTAT="/bin/netstat" #
+        CMDMKDIR="/usr/bin/mkdir" #
+        CMDMOUNT="/usr/bin/mount" #
+        CMDMV="/usr/bin/mv" #
+        CMDNETSTAT="/usr/bin/netstat" #
         CMDNGINX="/usr/sbin/nginx"
         CMDNTPDATE="/usr/sbin/ntpdate"
         CMDOPENSSL="/usr/bin/openssl"
         CMDPASTEBINIT="/usr/bin/pastebinit"
         CMDPGREP="/usr/bin/pgrep"
         CMDPHP="/usr/bin/php"
-        CMDPIP="/usr/bin/pip"
+        CMDPIP="/usr/bin/pip3"
         CMDPKILL="/usr/bin/pkill"
-        CMDPS="/bin/ps" #
+        CMDPS="/usr/bin/ps" #
         CMDPV="/usr/bin/pv"
         CMDRAR="/usr/bin/rar"
-        CMDRM="/bin/rm" #
+        CMDRM="/usr/bin/rm" #
         CMDRTORRENT="/usr/local/bin/rtorrent"
-        CMDSED="/bin/sed" #
+        CMDSED="/usr/bin/sed" #
         CMDSTAT="/usr/bin/stat"
-        CMDSU="/bin/su" #
-        CMDSYSTEMCTL="/bin/systemctl" #
+        CMDSU="/usr/bin/su" #
+        CMDSYSTEMCTL="/usr/bin/systemctl" #
         CMDTAIL="/usr/bin/tail"
-        CMDTAR="/bin/tar" #
+        CMDTAR="/usr/bin/tar" #
         CMDTEE="/usr/bin/tee"
         CMDTOUCH="/usr/bin/touch"
         CMDTR="/usr/bin/tr"
-        CMDTRUE="/bin/true" #
-        CMDTUNE2FS="/sbin/tune2fs" #
-        CMDUNAME="/bin/uname" #
+        CMDTRUE="/usr/bin/true" #
+        CMDTUNE2FS="/usr/sbin/tune2fs" #
+        CMDUNAME="/usr/bin/uname" #
         CMDUNZIP="/usr/bin/unzip"
         CMDUPDATERC="/usr/sbin/update-rc.d"
         CMDUSERADD="/usr/sbin/useradd"
@@ -76,8 +76,8 @@ if [[ "$CMDPATH" == stretch ]]  || [ ! -f "/usr/bin/bash" ]; then
         CMDXARGS="/usr/bin/xargs"
         CMDZIP="/usr/bin/zip"
 
-elif [[ "$CMDPATH" == buster ]]; then
-        # variables cmd buster
+elif [[ "$CMDPATH" == bullseye ]]; then
+        # variables cmd bullseye
         CMDAPTGET="/usr/bin/apt-get"
         CMDAPTKEY="/usr/bin/apt-key"
         CMDAWK="/usr/bin/awk"
